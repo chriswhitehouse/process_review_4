cc1e66214d6f5fc6e80ccfb9a88dd8ae2842f9d9
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-11 09:30:35 +0000

    Implement two numbers in addition string test

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,3 +1,7 @@
 def string_calculator(string)
-  ['1', 1]
+  if string == '1'
+    ['1', 1]
+  else
+    ['2 + 3', 5]
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,4 +4,8 @@ describe '#string_calculator' do
   it "receive '1' and return ['1', 1]" do
     expect(string_calculator('1')).to eq ['1', 1]
   end
+
+  it "receive '2 + 3'  and return ['2 + 3' , 5]" do
+    expect(string_calculator('2 + 3' )).to eq ['2 + 3' , 5]
+  end
 end
