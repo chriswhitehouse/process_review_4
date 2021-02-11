92aba4e1879f3ba1414ebb75b5b1611b36dc1d51
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-11 09:49:39 +0000

    Refactor three number addition test cases

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,9 +1,13 @@
 def string_calculator(string)
-  if string == '1'
-    ['1', 1]
-  elsif string == '2 + 3'
-    ['2 + 3', 5]
-  elsif string == '4 + 5 + 6'
-    ['4 + 5 + 6', 15]
+  input_array = string.split(' ')
+  result = 0
+
+  input_array.each_with_index do |val, index |
+    if index.even?
+      result += val.to_i
+    end
   end
+
+  return [string, result]
+
 end
