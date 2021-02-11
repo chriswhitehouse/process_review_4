ebe9dc23ad837186d3d628ee2cb00133d3230482
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-11 11:31:33 +0000

    Present where I got to in the review

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,6 +8,7 @@ describe '#string_calculator' do
   it "receive '2 + 3'  and return ['2 + 3' , 5]" do
     expect(string_calculator('2 + 3')).to eq ['2 + 3' , 5]
   end
+
   it "receive '4 + 5 + 6'  and return ['4 + 5 + 6' , 15]" do
     expect(string_calculator('4 + 5 + 6')).to eq ['4 + 5 + 6' , 15]
   end
